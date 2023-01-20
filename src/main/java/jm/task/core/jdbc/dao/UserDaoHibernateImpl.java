f007e38d516e66d32491106523173588f500580a
LlqIv <ivanlalkq@gmail.com>
2023-01-20 14:20:22 +0300

    70% of task

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,41 +1,102 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final SessionFactory sessionFactory = Util.getSessionFactory();
     public UserDaoHibernateImpl() {
     }
 
 
     @Override
     public void createUsersTable() {
+        try (var session = sessionFactory.openSession()) {
 
+            String sql = """
+
+            """;
+
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (var session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users_table").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
 
+            session.save(new User(name, lastName, age));
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
 
+            session.createQuery("DELETE User WHERE id = " + id);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            return session.createQuery("from User", User.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (var session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
 
+            session.createQuery("DELETE FROM User");
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
     }
 }
